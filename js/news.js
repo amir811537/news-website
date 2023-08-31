6983dfcce9a21e998fc4df971973712367381371
amir811537 <amirhossain.bc.75@gmail.com>
2023-08-31 10:07:09 +0600

    read more and less added

--- js/news.js
+++ js/news.js
@@ -19,29 +19,47 @@ const handleLoadNews = async (categoryId) => {
     try {
         const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
         const data = await res.json();
-        
+
         const cardContainer = document.getElementById('card-container');
-        
+
         cardContainer.innerHTML = ''; // Clear existing content
-        
+
         data.data.forEach((news) => {
             const div = document.createElement('div');
-            div.innerHTML = `<div class="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="${news.image_url}" alt="${news.title}" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">${news.title}</h2>
-                
-                <p>Author: ${news.author.name}</p>
-                <p>Published Date: ${news.author.published_date}</p>
-                <p>Category ID: ${news.category_id}</p>
-                <p>Rating: ${news.rating.number} (${news.rating.badge})</p>
-                <p>Total Views: ${news.total_view}</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Read More</button>
-                </div>
-            </div>
-        </div>`;
+            div.innerHTML = `
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="${news.image_url}" alt="${news.title}" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">${news.title}</h2>
+
+                        <p><span class="font-bold">Author: </span>${news.author.name}</p>
+                        <p><span class="font-bold">Published Date:</span> ${news.author.published_date}</p>
+                        <p>Rating: ${news.rating.number} (${news.rating.badge})</p>
+                        <p>Total Views: ${news.total_view}</p>
+
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary read-more-btn">Read More</button>
+                        </div>
+
+                        <div class="details" style="display: none;">
+                            <p><span class="font-bold"></span> ${news.details}</p>
+                        </div>
+                    </div>
+                </div>`;
             cardContainer.appendChild(div);
+
+            const readMoreButton = div.querySelector('.read-more-btn');
+            const detailsDiv = div.querySelector('.details');
+
+            readMoreButton.addEventListener('click', () => {
+                if (detailsDiv.style.display === 'none') {
+                    detailsDiv.style.display = 'block';
+                    readMoreButton.textContent = 'Read Less';
+                } else {
+                    detailsDiv.style.display = 'none';
+                    readMoreButton.textContent = 'Read More';
+                }
+            });
         });
 
         console.log(data);
@@ -51,4 +69,3 @@ const handleLoadNews = async (categoryId) => {
 };
 
 handleCategory();
-{/* <p>amir${news.details}</p> */}
